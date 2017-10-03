a15ae956a6f9dfed247399eeaea29f4409111bd8
Timothy Burchfield <timothy.burchfield16@gmail.com>
2017-10-03 14:46:17 -0400

    Add the VGA wrappers

--- miniproject.v
+++ miniproject.v
@@ -1,4 +1,38 @@
 module miniproject(
-);
+	input KEY, // KEY[i] refers to ith key
+	input SW[2:0],
+	input CLOCK_50,
+	output			VGA_CLK,   				//	VGA Clock
+	output			VGA_HS,					//	VGA H_SYNC
+	output			VGA_VS,					//	VGA V_SYNC
+	output			VGA_BLANK,				//	VGA BLANK
+	output			VGA_SYNC,				//	VGA SYNC
+	output [9:0]	VGA_R,   				//	VGA Red[9:0]
+	output [9:0]	VGA_G,	 				//	VGA Green[9:0]
+	output [9:0]	VGA_B	   				//	VGA Blue[9:0]
+	);
+
+	vga_adapter VGA(
+		.resetn		(KEY[1]),
+		.clock		(CLOCK_50),
+		.colour		(SW[2:0]),
+		.x				(SW[17:10]),
+		.y				(SW[9:3]),
+		.plot			(~KEY[0]),
+		/* Signals for the DAC to drive the monitor. */
+		.VGA_R		(VGA_R),
+		.VGA_G		(VGA_G),
+		.VGA_B		(VGA_B),
+		.VGA_HS		(VGA_HS),
+		.VGA_VS		(VGA_VS),
+		.VGA_BLANK	(VGA_BLANK),
+		.VGA_SYNC	(VGA_SYNC),
+		.VGA_CLK		(VGA_CLK)
+	);
+	
+	defparam VGA.RESOLUTION = "160x120";
+	defparam VGA.MONOCHROME = "FALSE";
+	defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+	defparam VGA.BACKGROUND_IMAGE = "background_image.mif";
 
 endmodule
